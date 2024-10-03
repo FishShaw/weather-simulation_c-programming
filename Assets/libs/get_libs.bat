8e666d849529c37fac9813b482bcc16bec856e69
bartjuhhh <bart.knuiman@wur.nl>
2024-10-03 17:22:32 +0200

    Initial assets and 2 sample scenes.

---
+++ Assets/libs/get_libs.bat
@@ -0,0 +1,8 @@
+git clone git@git.wur.nl:wander/3dbuildings.git
+git clone git@git.wur.nl:wander/terrain.git
+git clone git@git.wur.nl:wander/utils.git
+git clone git@git.wur.nl:wander/trees.git
+git clone git@git.wur.nl:wander/geotiff.git
+git clone git@git.wur.nl:wander/underground.git
+git clone git@git.wur.nl:wander/vectortile.git
+git clone git@git.wur.nl:wander/tramsbusses.git
