753a3f021ba6d5b13bb15cad9b05115458b52ebb
Fish Shaw <964548483@qq.com>
2024-12-16 18:01:13 +0100

    update on TimeController and TimelineUI

--- Assets/libs/WeatherSystemBuilt/Scripts/TimeManagement/TimeController.cs
+++ Assets/libs/WeatherSystemBuilt/Scripts/TimeManagement/TimeController.cs
@@ -1,8 +1,117 @@
 using UnityEngine;
+using System;
+using WeatherSystem.Core;
+using WeatherSystem.DataHandling;
 
 namespace WeatherSystem.TimeManagement
 {
     public class TimeController : MonoBehaviour
     {
+        [SerializeField] private WeatherSettings settings;
+        [SerializeField] private WeatherDataLoader dataLoader;
+        
+        private DateTime currentTime;
+        private DateTime startTime;
+        private DateTime endTime;
+        private bool isPlaying = false;
+        private float playbackSpeed = 1.0f;
+        private float accumulatedTime = 0f;
+
+        public event Action<DateTime> OnTimeChanged;
+        public event Action<bool> OnPlaybackStateChanged;
+
+        private void Start()
+        {
+            if (dataLoader == null)
+            {
+                dataLoader = FindObjectOfType<WeatherDataLoader>();
+            }
+
+            // Initialize with the data loader's time range
+            startTime = new DateTime(2024, 7, 9, 12, 0, 0);
+            endTime = new DateTime(2024, 7, 10, 4, 0, 0);
+            currentTime = startTime;
+        }
+
+        private void Update()
+        {
+            if (isPlaying)
+            {
+                accumulatedTime += Time.deltaTime * playbackSpeed;
+                if (accumulatedTime >= settings.updateInterval)
+                {
+                    accumulatedTime = 0f;
+                    AdvanceTime();
+                    WeatherManager.Instance.UpdateWeatherData();
+                }
+            }
+        }
+
+        private void AdvanceTime()
+        {
+            currentTime = currentTime.AddMinutes(1);
+            if (currentTime > endTime)
+            {
+                currentTime = startTime;
+            }
+            OnTimeChanged?.Invoke(currentTime);
+        }
+
+        public void Play()
+        {
+            isPlaying = true;
+            OnPlaybackStateChanged?.Invoke(isPlaying);
+        }
+
+        public void Pause()
+        {
+            isPlaying = false;
+            OnPlaybackStateChanged?.Invoke(isPlaying);
+        }
+
+        public void SetTime(DateTime newTime)
+        {
+            if (newTime >= startTime && newTime <= endTime)
+            {
+                currentTime = newTime;
+                OnTimeChanged?.Invoke(currentTime);
+            }
+        }
+
+        public enum PlaybackSpeed
+        {
+            Normal = 1,
+            Double = 2,
+            Fast = 5,
+            VeryFast = 10
+        }
+
+        private PlaybackSpeed currentSpeed = PlaybackSpeed.Normal;
+        
+        public void SetPlaybackSpeed(PlaybackSpeed speed)
+        {
+            currentSpeed = speed;
+            playbackSpeed = (float)speed;
+            OnPlaybackSpeedChanged?.Invoke((int)currentSpeed);
+        }
+
+        public PlaybackSpeed GetCurrentSpeed()
+        {
+            return currentSpeed;
+        }
+
+        public event Action<int> OnPlaybackSpeedChanged;
+
+        public DateTime GetCurrentTime()
+        {
+            return currentTime;
+        }
+
+        public float GetNormalizedTime()
+        {
+            TimeSpan totalDuration = endTime - startTime;
+            TimeSpan currentDuration = currentTime - startTime;
+            return (float)(currentDuration.TotalMinutes / totalDuration.TotalMinutes);
+        }
     }
 } 
--- Assets/libs/WeatherSystemBuilt/Scripts/TimeManagement/TimelineUI.cs
+++ Assets/libs/WeatherSystemBuilt/Scripts/TimeManagement/TimelineUI.cs
@@ -1,8 +1,106 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 namespace WeatherSystem.TimeManagement
 {
     public class TimelineUI : MonoBehaviour
     {
+        [Header("UI References")]
+        [SerializeField] private Slider timelineSlider;
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button pauseButton;
+        [SerializeField] private TMP_Text timeText;
+        [SerializeField] private TMP_Text speedText;
+        [SerializeField] private Button[] speedButtons;
+
+        [Header("Components")]
+        [SerializeField] private TimeController timeController;
+
+        private int[] speedValues = { 2, 5, 10 };
+
+        private void Start()
+        {
+            if (timeController == null)
+            {
+                timeController = FindObjectOfType<TimeController>();
+            }
+
+            SetupUIElements();
+            SubscribeToEvents();
+        }
+
+        private void SetupUIElements()
+        {
+            timelineSlider.onValueChanged.AddListener(OnSliderValueChanged);
+            playButton.onClick.AddListener(OnPlayClicked);
+            pauseButton.onClick.AddListener(OnPauseClicked);
+            SetupSpeedButtons();
+
+            UpdateTimeDisplay(timeController.GetCurrentTime());
+            UpdateSpeedText((int)timeController.GetCurrentSpeed());
+        }
+
+        private void SubscribeToEvents()
+        {
+            timeController.OnTimeChanged += UpdateTimeDisplay;
+            timeController.OnPlaybackStateChanged += UpdatePlaybackUI;
+            timeController.OnPlaybackSpeedChanged += UpdateSpeedText;
+        }
+
+        private void OnSliderValueChanged(float value)
+        {
+            DateTime startTime = new DateTime(2024, 7, 9, 12, 0, 0);
+            DateTime endTime = new DateTime(2024, 7, 10, 4, 0, 0);
+            TimeSpan totalDuration = endTime - startTime;
+            
+            DateTime newTime = startTime.AddMinutes(totalDuration.TotalMinutes * value);
+            timeController.SetTime(newTime);
+        }
+
+        private void UpdateTimeDisplay(DateTime time)
+        {
+            timeText.text = time.ToString("MM/dd HH:mm");
+            timelineSlider.value = timeController.GetNormalizedTime();
+        }
+
+        private void UpdatePlaybackUI(bool isPlaying)
+        {
+            playButton.gameObject.SetActive(!isPlaying);
+            pauseButton.gameObject.SetActive(isPlaying);
+        }
+
+        private void OnPlayClicked() => timeController.Play();
+        private void OnPauseClicked() => timeController.Pause();
+
+        private void SetupSpeedButtons()
+        {
+            for (int i = 0; i < speedButtons.Length; i++)
+            {
+                int speedIndex = i;
+                speedButtons[i].onClick.AddListener(() => OnSpeedButtonClicked(speedValues[speedIndex]));
+            }
+        }
+
+        private void OnSpeedButtonClicked(int speedValue)
+        {
+            timeController.SetPlaybackSpeed((TimeController.PlaybackSpeed)speedValue);
+            UpdateSpeedText(speedValue);
+        }
+
+        private void UpdateSpeedText(int speed)
+        {
+            speedText.text = speed == 1 ? "normal" : $"{speed}x";
+        }
+
+        private void OnDestroy()
+        {
+            if (timeController != null)
+            {
+                timeController.OnTimeChanged -= UpdateTimeDisplay;
+                timeController.OnPlaybackStateChanged -= UpdatePlaybackUI;
+            }
+        }
     }
 } 
